e60166b0cdb41011c2d3eef39b5f88e8c580e81a
Gavin <1146855407@qq.com>
2018-09-10 21:48:31 +0800

    6-12 JsonInclude 如何从废弃的方法里找到最新的方法。

--- src/main/java/com/sell/dto/OrderDTO.java
+++ src/main/java/com/sell/dto/OrderDTO.java
@@ -1,14 +1,18 @@
 package com.sell.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.sell.dataobject.OrderDetail;
 
+import com.sell.utils.serialize.Date2Serializer;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 @Data
-
+//@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
 
     private String orderId;
@@ -28,9 +32,11 @@ public class OrderDTO {
     private Integer payStatus ;
 
     @org.hibernate.annotations.CreationTimestamp
+    @JsonSerialize(using = Date2Serializer.class)
     private Date createTime;
 
     @org.hibernate.annotations.UpdateTimestamp
+    @JsonSerialize(using = Date2Serializer.class)
     private Date updateTime;
 
     private List<OrderDetail> orderDetailList;
