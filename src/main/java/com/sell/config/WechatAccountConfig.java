e6073e11fd45e35556ac0241f2952a69e43909d6
Gavin <1146855407@qq.com>
2018-09-13 01:48:50 +0800

    7-5 微信网页授权 注意要结合8-8来看， 使用微信MP SDK

---
+++ src/main/java/com/sell/config/WechatAccountConfig.java
@@ -0,0 +1,13 @@
+package com.sell.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix="wechat")
+public class WechatAccountConfig {
+    private String mpAppId;
+    private String mpAppSecret;
+}
--- src/main/java/com/sell/controller/WeixinController.java
+++ src/main/java/com/sell/controller/WeixinController.java
@@ -1,10 +1,11 @@
-package com.sell.controller;
+//package com.sell.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/weixin")
 @RestController
@@ -14,6 +15,11 @@ public class WeixinController {
     @GetMapping("/auth")
     public void auth(@RequestParam("code")String code){
         log.info("【进入auth】....");
+
         log.info("【获取code】code={}",code);
+        String url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx68c1e83f731d1c75&secret=6363c7aa4178ac8669c3565c0ec2421b&code="+code+"&grant_type=authorization_code";
+        RestTemplate restTemplate=new RestTemplate();
+        String response=restTemplate.getForObject(url,String.class);
+        log.info("【获取token】response={}",response);
     }
 }
